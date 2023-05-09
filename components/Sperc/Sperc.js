906f85b05d92fdc9b04e9f759ed5abf0f2814ec2
benji <benjipictor@gmail.com>
2023-05-09 11:15:45 +0100

    first commit

---
+++ components/Sperc/Sperc.js
@@ -0,0 +1,20 @@
+const 
+
+function Sperc() {
+
+
+    return (
+        <header className="Section-header">
+            <h2>Sensory Processing and Early Relationships</h2>
+            <p>
+                Information for this section was provided through questionnaires completed by Frankie’s parents and teachers.
+                Answers have been colour coded, indicating who has highlighted this response:
+            </p>
+            <ul className="UL-noStyle">
+                <li className="Blue-text">Parent</li>
+                <li className="Orange-text">Teacher</li>
+                <li className="Green-text">Parent and Teacher</li>
+            </ul>
+        </header>
+    )
+}
