2803bba01151f03f8514b7a11c9b073bb9399d83
benji <benjipictor@gmail.com>
2023-05-17 13:25:30 +0100

    finished checklist item

--- doc-server/controllers/assesment.js
+++ doc-server/controllers/assesment.js
@@ -1,17 +1,17 @@
 import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
-import { Prisma } from '@prisma/client'
 import { createItems } from '../domain/assesment.js'
 export const submitChecklistItems = async (req, res) => {
-    
+    const arrayItems = req.body
+    arrayItems.forEach(item => {
+        if (!item.hasOwnProperty("itemNumber", "description", "checklistId", "response")) {
+            return sendDataResponse(res, 404, {error: "Keys do not match the expected format", object: item})
+        }
+    });
     try {
         const createdItems = await createItems(req.body)
         return sendDataResponse(res, 200, createdItems)
     } catch (e) {
-        console.log(e)
-        if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            console.log(e)
-
-        }
+        
         sendDataResponse(res, 500, {error: e})
     }
 }
